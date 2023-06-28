0bb64f7788cbf0e47b50a0baf4eff4983b4115f6
Zeyna Baaj <zainabaaj@gmail.com>
2023-06-28 18:54:39 +0300

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return <div>Naturally,  I agree with this article.</div>;
 }
 
 export default Comment;
